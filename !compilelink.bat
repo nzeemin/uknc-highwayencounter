7c4d628ee3a0debb1cc2b2e752a44d268570dcfa
Nikita Zimin <nzeemin@gmail.com>
2017-04-14 15:39:40 +0300

    Print version in Small Font

--- !compilelink.bat
+++ !compilelink.bat
@@ -2,12 +2,11 @@
 
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
-set "DATESTAMP=%YYYY%-%MM%-%DD%"
+set "DATESTAMP=%YYYY%:%MM%:%DD%"
 for /f %%i in ('git rev-list HEAD --count') do (set REVISION=%%i)
 echo REV.%REVISION% %DATESTAMP%
 
-echo VERSTR:	.ASCIZ /REV.%REVISION% %DATESTAMP%/ > VERSIO.MAC
-echo	.EVEN >> VERSIO.MAC
+echo VERSTR:	.ASCII /REV;%REVISION%@%DATESTAMP%/ > VERSIO.MAC
 
 @if exist TILES.OBJ del TILES.OBJ
 @if exist HWYENC.LST del HWYENC.LST
